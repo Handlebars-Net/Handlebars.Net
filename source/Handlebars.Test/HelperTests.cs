22a8b17f4c89acc46505d03833abc37ddeab6eb2
Rex M <rexm@Rex-Morgans-MacBook-Pro.local>
2014-06-29 15:21:51 -0400

    Test for quoted literals

---
+++ source/Handlebars.Test/HelperTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using Handlebars;
+
+namespace Handlebars.Test
+{
+    [TestFixture]
+    public class HelperTests
+    {
+        [Test]
+        public void HelperWithLiteralArguments()
+        {
+            Handlebars.RegisterHelper("myHelper", (writer, context, args) => {
+                var count = 0;
+                foreach(var arg in args)
+                {
+                    writer.Write("\nThing {0}: {1}", ++count, arg);
+                }
+            });
+
+            var source = "Here are some things: {{myHelper 'foo' 'bar'}}";
+
+            var template = Handlebars.Compile(source);
+
+            var output = template(new { });
+
+            var expected = "Here are some things: \nThing 1: foo\nThing 2: bar";
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void HelperWithLiteralArgumentsWithQuotes()
+        {
+            var helperName = Guid.NewGuid().ToString(); //randomize helper name
+            Handlebars.RegisterHelper(helperName, (writer, context, args) => {
+                var count = 0;
+                foreach(var arg in args)
+                {
+                    writer.WriteSafeString(
+                        string.Format("\nThing {0}: {1}", ++count, arg));
+                }
+            });
+
+            var source = "Here are some things: {{" + helperName + " 'My \"favorite\" movie' 'bar'}}";
+
+            var template = Handlebars.Compile(source);
+
+            var output = template(new { });
+
+            var expected = "Here are some things: \nThing 1: My \"favorite\" movie\nThing 2: bar";
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
+
