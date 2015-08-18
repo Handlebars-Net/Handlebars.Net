039966012f8bb4a638eecf321e5a7084e39e7e86
mcintyre321 <mcintyre321@gmail.com>
2015-08-18 18:44:42 +0100

    Added dyanmic viewmodel in order to pass data to layout

--- source/Handlebars/Compiler/HandlebarsCompiler.cs
+++ source/Handlebars/Compiler/HandlebarsCompiler.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using HandlebarsDotNet.Compiler.Lexer;
 
@@ -55,12 +58,57 @@ namespace HandlebarsDotNet.Compiler
                     compiledView(innerWriter, vm);
                 }
                 var inner = sb.ToString();
-                compiledLayout(tw, new {body = inner});
+                compiledLayout(tw, new DynamicViewModel(new {body = inner}));
             };
         }
 
-    
-        
+
+        internal class DynamicViewModel : DynamicObject
+        {
+            private readonly object[] _objects;
+            private static readonly BindingFlags BindingFlags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase;
+
+            public DynamicViewModel(params object[] objects)
+            {
+                _objects = objects;
+            }
+
+
+            public override IEnumerable<string> GetDynamicMemberNames()
+            {
+                return _objects.Select(o => o.GetType())
+                    .SelectMany(t => t.GetMembers(BindingFlags))
+                    .Select(m => m.Name);
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                result = null;
+                foreach (var target in _objects)
+                {
+                    var member = target.GetType()
+                        .GetMember(binder.Name, BindingFlags);
+
+                    if (member.Length > 0)
+                    {
+                        if (member[0] is PropertyInfo)
+                        {
+                            result = ((PropertyInfo)member[0]).GetValue(target, null);
+                            return true;
+                        }
+                        if (member[0] is FieldInfo)
+                        {
+                            result = ((FieldInfo)member[0]).GetValue(target);
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
     }
+
+ 
 }
 
