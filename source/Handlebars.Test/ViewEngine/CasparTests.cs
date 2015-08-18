3a618cd34272fbff916c1dbb50d0b4f4e2295196
mcintyre321 <mcintyre321@gmail.com>
2015-08-18 17:20:36 +0100

    Change namespace to avoid conflict with class

--- source/Handlebars.Test/ViewEngine/CasparTests.cs
+++ source/Handlebars.Test/ViewEngine/CasparTests.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CsQuery;
-using HandlebarsDotNet;
+﻿using System.IO;
 using NUnit.Framework;
-using Handlebars = HandlebarsDotNet.Handlebars;
 
-namespace Handlebars.Test.ViewEngine
+namespace HandlebarsDotNet.Test.ViewEngine
 {
     [TestFixture]
     public class CasparTests
