0f9dfe805ec5187c0d8d679df68d0e3b899c2e28
Gabriel Lemire <gabriel.lemire@gmail.com>
2016-01-26 23:39:15 -0500

    Started creating performance test for Handlebars.Net

---
+++ source/Handlebars.PerfTests/Models/DataModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Handlebars.PerfTests.Models
+{
+    public class DataModel
+    {
+        public List<Model> Data { get; set; }
+
+        public static DataModel GetRandom(int times)
+        {
+            var obj = new DataModel()
+            {
+                Data = new List<Model>()
+            };
+
+            for (int i = 0; i < times; i++)
+            {
+                obj.Data.Add(Model.CreateRandom());
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ source/Handlebars.PerfTests/Models/Model.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Exceptionless;
+
+namespace Handlebars.PerfTests.Models
+{
+    public class Model
+    {
+        public Obj Obj { get; set; }
+
+        public Dictionary<string, object> HashMap { get; set; }
+
+        public dynamic DynamicObject { get; set; }
+
+        public Dictionary<int, double> DoubleDict { get; set; }
+
+        public List<string> Coll { get; set; }
+
+        public static Model CreateRandom()
+        {
+            var obj = new Model()
+            {
+                Obj = Obj.CreateRandom(),
+                HashMap = HashMapExtension.CreateRandomHashMap(),
+                DynamicObject = new ExpandoObject(),
+                DoubleDict = new Dictionary<int, double>
+                {
+                    { 0, double.Epsilon },
+                    { 1, Math.PI },
+                    { 2, 0.0 }
+                },
+                Coll = new List<string>()
+                {
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString(),
+                    RandomData.GetAlphaString()
+                }
+            };
+
+            obj.DynamicObject.Alpha = RandomData.GetAlphaString();
+            obj.DynamicObject.Beta = RandomData.GetInt();
+            obj.DynamicObject.Gamma = new Guid();
+            obj.DynamicObject.Delta = new object();
+            obj.DynamicObject.Epsilon = RandomData.GetLong();
+
+            return obj;
+        }
+
+
+    }
+}
