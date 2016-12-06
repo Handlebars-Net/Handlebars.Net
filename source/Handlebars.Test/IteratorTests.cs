5761611d366847d21eb475ce21118e6d61afd446
Nikita Kotlyarov <abraham-fox@users.noreply.github.com>
2016-12-06 17:25:13 +0400

    Fix issue with getting a variable from a parent context (#174)
    * Fix issue with getting a variable from a parent context
    
    Issue #107
    
    * Replace tabs by spaces

--- source/Handlebars.Test/IteratorTests.cs
+++ source/Handlebars.Test/IteratorTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace HandlebarsDotNet.Test
 {
@@ -44,6 +43,140 @@ namespace HandlebarsDotNet.Test
                 };
             var result = template(data);
             Assert.AreEqual("Hello,\n0. Erik\n1. Helen", result);
+        }
+
+        [Test]
+        public void WithParentIndex()
+        {
+            var source = @"
+                {{#each level1}}
+                    id={{id}}
+                    index=[{{@../../index}}:{{@../index}}:{{@index}}]
+                    first=[{{@../../first}}:{{@../first}}:{{@first}}]
+                    last=[{{@../../last}}:{{@../last}}:{{@last}}]
+                    {{#each level2}}
+                        id={{id}}
+                        index=[{{@../../index}}:{{@../index}}:{{@index}}]
+                        first=[{{@../../first}}:{{@../first}}:{{@first}}]
+                        last=[{{@../../last}}:{{@../last}}:{{@last}}]
+                        {{#each level3}}
+                            id={{id}}
+                            index=[{{@../../index}}:{{@../index}}:{{@index}}]
+                            first=[{{@../../first}}:{{@../first}}:{{@first}}]
+                            last=[{{@../../last}}:{{@../last}}:{{@last}}]
+                        {{/each}}
+                    {{/each}}    
+                {{/each}}";
+            var template = Handlebars.Compile( source );
+            var data = new
+                {
+                    level1 = new[]{
+                        new {
+                            id = "0",
+                            level2 = new[]{
+                                new {
+                                    id = "0-0",
+                                    level3 = new[]{
+                                        new { id = "0-0-0" },
+                                        new { id = "0-0-1" }
+                                    }
+                                },
+                                new {
+                                    id = "0-1",
+                                    level3 = new[]{
+                                        new { id = "0-1-0" },
+                                        new { id = "0-1-1" }
+                                    }
+                                }
+                            }
+                        },
+                        new {
+                            id = "1",
+                            level2 = new[]{
+                                new {
+                                    id = "1-0",
+                                    level3 = new[]{
+                                        new { id = "1-0-0" },
+                                        new { id = "1-0-1" }
+                                    }
+                                },
+                                new {
+                                    id = "1-1",
+                                    level3 = new[]{
+                                        new { id = "1-1-0" },
+                                        new { id = "1-1-1" }
+                                    }
+                                }
+                            }
+                        }
+                    }
+            };
+
+            var result = template( data );
+
+            const string expected = @"
+                            id=0
+                            index=[::0]
+                            first=[::True]
+                            last=[::False]
+                                id=0-0
+                                index=[:0:0]
+                                first=[:True:True]
+                                last=[:False:False]
+                                    id=0-0-0
+                                    index=[0:0:0]
+                                    first=[True:True:True]
+                                    last=[False:False:False]
+                                    id=0-0-1
+                                    index=[0:0:1]
+                                    first=[True:True:False]
+                                    last=[False:False:True]
+                                id=0-1
+                                index=[:0:1]
+                                first=[:True:False]
+                                last=[:False:True]
+                                    id=0-1-0
+                                    index=[0:1:0]
+                                    first=[True:False:True]
+                                    last=[False:True:False]
+                                    id=0-1-1
+                                    index=[0:1:1]
+                                    first=[True:False:False]
+                                    last=[False:True:True]
+                            id=1
+                            index=[::1]
+                            first=[::False]
+                            last=[::True]
+                                id=1-0
+                                index=[:1:0]
+                                first=[:False:True]
+                                last=[:True:False]
+                                    id=1-0-0
+                                    index=[1:0:0]
+                                    first=[False:True:True]
+                                    last=[True:False:False]
+                                    id=1-0-1
+                                    index=[1:0:1]
+                                    first=[False:True:False]
+                                    last=[True:False:True]
+                                id=1-1
+                                index=[:1:1]
+                                first=[:False:False]
+                                last=[:True:True]
+                                    id=1-1-0
+                                    index=[1:1:0]
+                                    first=[False:False:True]
+                                    last=[True:True:False]
+                                    id=1-1-1
+                                    index=[1:1:1]
+                                    first=[False:False:False]
+                                    last=[True:True:True]";
+
+            // Console.WriteLine(result); 
+
+            Func<string, string> makeFlat = text => text.Replace( " ", "" ).Replace( "\n", "" ).Replace( "\r", "" );
+
+            Assert.AreEqual( makeFlat( expected ), makeFlat( result ) );
         }
 
         [Test]
--- source/Handlebars/Compiler/Translation/Expression/PathBinder.cs
+++ source/Handlebars/Compiler/Translation/Expression/PathBinder.cs
@@ -138,6 +138,16 @@ namespace HandlebarsDotNet.Compiler
         //TODO: make path resolution logic smarter
         private object ResolvePath(BindingContext context, string path)
         {
+            var containsVariable = path.StartsWith( "@" );
+            if(containsVariable)
+            {
+                path = path.Substring( 1 );
+                if(path.Contains( ".." ))
+                {
+                    context = context.ParentContext;
+                }
+            }
+
             var instance = context.Value;
             var hashParameters = instance as HashParameterDictionary;
 
@@ -148,13 +158,17 @@ namespace HandlebarsDotNet.Compiler
                     context = context.ParentContext;
                     if (context == null)
                     {
+                        if (containsVariable) return string.Empty;
+                        
                         throw new HandlebarsCompilerException("Path expression tried to reference parent of root");
                     }
                     instance = context.Value;
                 }
                 else
                 {
-                    foreach (var memberName in segment.Split('.'))
+                    var objectPropertiesChain = containsVariable ? "@" + segment:  segment;
+
+                    foreach (var memberName in objectPropertiesChain.Split('.'))
                     {
                         instance = ResolveValue(context, instance, memberName);
 
@@ -183,7 +197,7 @@ namespace HandlebarsDotNet.Compiler
 
         private object ResolveValue(BindingContext context, object instance, string segment)
         {
-	        object resolvedValue = new UndefinedBindingResult( segment, CompilationContext.Configuration );
+            object resolvedValue = new UndefinedBindingResult( segment, CompilationContext.Configuration );
             if (segment.StartsWith("@"))
             {
                 var contextValue = context.GetContextVariable(segment.Substring(1));
