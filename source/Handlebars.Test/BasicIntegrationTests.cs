06f8e05adddf018197ecf85a58ef10aa2368d562
Valentin Zwick <vzwick@users.noreply.github.com>
2017-03-07 19:35:12 +0100

    Well, no LINQ then.

--- source/Handlebars.Test/BasicIntegrationTests.cs
+++ source/Handlebars.Test/BasicIntegrationTests.cs
@@ -872,7 +872,7 @@ namespace HandlebarsDotNet.Test
             var source = @"{{verbatim '{{foo}}'}} something {{verbatim '{{bar}}'}}";
 
             Handlebars.RegisterHelper("verbatim",
-                (writer, context, args) => writer.WriteSafeString(args.FirstOrDefault()));
+                (writer, context, args) => writer.Write(args[0])));
 
             var template = Handlebars.Compile(source);
             
