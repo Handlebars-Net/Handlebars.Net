75e1af9ad9d86b38553cd07ec89feb7ad6f481b1
John Drago <jdragoPassport@eprojects247.com>
2018-10-05 18:00:06 -0400

    Issue 270, Inline templates not usable inside #each

--- source/Handlebars/Compiler/Translation/Expression/IteratorBinder.cs
+++ source/Handlebars/Compiler/Translation/Expression/IteratorBinder.cs
@@ -310,7 +310,7 @@ namespace HandlebarsDotNet.Compiler
         private class IteratorBindingContext : BindingContext
         {
             public IteratorBindingContext(BindingContext context)
-                : base(context.Value, context.TextWriter, context.ParentContext, context.TemplatePath)
+                : base(context.Value, context.TextWriter, context.ParentContext, context.TemplatePath, context.InlinePartialTemplates)
             {
             }
 
@@ -324,7 +324,7 @@ namespace HandlebarsDotNet.Compiler
         private class ObjectEnumeratorBindingContext : BindingContext
         {
             public ObjectEnumeratorBindingContext(BindingContext context)
-                : base(context.Value, context.TextWriter, context.ParentContext, context.TemplatePath)
+                : base(context.Value, context.TextWriter, context.ParentContext, context.TemplatePath, context.InlinePartialTemplates)
             {
             }
 
