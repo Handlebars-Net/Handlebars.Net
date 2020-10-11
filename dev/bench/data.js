99c5ebe8d6aefaff6cc68702dc2e7e6628c0a6c1
github-action-benchmark <github@users.noreply.github.com>
2020-10-11 17:07:15 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 01ef770e6843d6779fbd6f3a7b700a6bf677bbe9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602361014110,
+  "lastUpdate": 1602436035879,
   "repoUrl": "https://github.com/Handlebars-Net/Handlebars.Net",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -96,6 +96,102 @@ window.BENCHMARK_DATA = {
             "value": 3860625.307198661,
             "unit": "ns",
             "range": "± 115303.22133554166"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zjklee@gmail.com",
+            "name": "Oleh Formaniuk",
+            "username": "zjklee"
+          },
+          "committer": {
+            "email": "zjklee@gmail.com",
+            "name": "Oleh Formaniuk",
+            "username": "zjklee"
+          },
+          "distinct": true,
+          "id": "01ef770e6843d6779fbd6f3a7b700a6bf677bbe9",
+          "message": "Update readme with CI/Sonar info",
+          "timestamp": "2020-10-11T20:01:59+03:00",
+          "tree_id": "0d58d05bcbeb7d049204ab484a9705b703014d56",
+          "url": "https://github.com/Handlebars-Net/Handlebars.Net/commit/01ef770e6843d6779fbd6f3a7b700a6bf677bbe9"
+        },
+        "date": 1602436035124,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "HandlebarsNet.Benchmark.Compilation.Template",
+            "value": 44911432.87777778,
+            "unit": "ns",
+            "range": "± 2111707.8229665207"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallHelperWithoutParameters",
+            "value": 130.25045762062072,
+            "unit": "ns",
+            "range": "± 8.27294590189249"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallHelperWithOneParameter",
+            "value": 127.30757771219525,
+            "unit": "ns",
+            "range": "± 4.932222737379302"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallHelperWithTwoParameter",
+            "value": 169.18157666524252,
+            "unit": "ns",
+            "range": "± 10.564743971908127"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.LateCallHelperWithoutParameters",
+            "value": 705.1620930989583,
+            "unit": "ns",
+            "range": "± 24.45441053526425"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.LateCallHelperWithOneParameter",
+            "value": 390.51982107162473,
+            "unit": "ns",
+            "range": "± 18.18713143375078"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.LateCallHelperWithTwoParameter",
+            "value": 417.1917410850525,
+            "unit": "ns",
+            "range": "± 15.708929864034964"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallBlockHelperWithoutParameters",
+            "value": 2221.6751643589564,
+            "unit": "ns",
+            "range": "± 66.20257217740578"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallBlockHelperWithOneParameter",
+            "value": 2244.7922439575195,
+            "unit": "ns",
+            "range": "± 51.438884645987635"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.Execution.CallBlockHelperWithTwoParameter",
+            "value": 2173.730542755127,
+            "unit": "ns",
+            "range": "± 94.81969256205772"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.EndToEnd.Default(N: 5, DataType: \"dictionary\")",
+            "value": 4160659.4972098214,
+            "unit": "ns",
+            "range": "± 116483.94963266149"
+          },
+          {
+            "name": "HandlebarsNet.Benchmark.EndToEnd.Default(N: 5, DataType: \"object\")",
+            "value": 4308724.955729167,
+            "unit": "ns",
+            "range": "± 182842.19893818576"
           }
         ]
       }
