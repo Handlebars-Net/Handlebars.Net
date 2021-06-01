5c7fc6d27a62b21b1edb06de96e2f05844cea3e4
Albert Schulz <aschulz@accemic.com>
2021-06-01 15:23:12 +0200

    Change set of array lengths for benchmark.

--- source/Handlebars.Benchmark/LargeArray.cs
+++ source/Handlebars.Benchmark/LargeArray.cs
@@ -10,27 +10,20 @@ namespace HandlebarsNet.Benchmark
     public class LargeArray
     {
         private object _data;
-        private HandlebarsTemplate<TextWriter, object, object> _default;
+        private HandlebarsTemplate<object, object> _default;
 
-        [Params(1, 2, 3, 4, 5)]
+        [Params(20000, 40000, 80000)]
         public int N { get; set; }
         
         [GlobalSetup]
         public void Setup()
         {
             const string template = @"{{#each this}}{{this}}{{/each}}";
-
-            var handlebars = Handlebars.Create();
-
-            using (var reader = new StringReader(template))
-            {
-                _default = handlebars.Compile(reader);
-            }
-
-            _data = Enumerable.Range(0, (int) Math.Pow(10, N)).ToList();
+            _default = Handlebars.Compile(template);
+            _data = Enumerable.Range(0, N).ToList();
         }
         
         [Benchmark]
-        public void Default() => _default(TextWriter.Null, _data);
+        public void Default() => _default(_data);
     }
 }
