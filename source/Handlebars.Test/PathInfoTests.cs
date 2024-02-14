dd45cbe3750b8cbe06beb78ffe2536d9b3f9296a
Hoeksema <7414068+Hoeksema@users.noreply.github.com>
2024-02-14 17:46:09 +0100

    Update PathInfoTests.cs
    complete test coverage

--- source/Handlebars.Test/PathInfoTests.cs
+++ source/Handlebars.Test/PathInfoTests.cs
@@ -52,6 +52,7 @@ namespace HandlebarsDotNet.Test
         [InlineData("a/[b.c/d]/e", new [] {"a", "[b.c/d]", "e"})]
         [InlineData("a/[b.c/d/e/f]/e", new [] {"a", "[b.c/d/e/f]", "e"})]
         [InlineData("a/[a//b/c.d/e]/e", new [] {"a", "[a//b/c.d/e]", "e"})]
+        [InlineData("a/[b/c/d].[e/f/g]/h", new [] {"a", "[b/c/d].[e/f/g]", "h"})]
         public void SlashPath(string input, string[] expected)
         {
             var pathInfo = PathInfo.Parse(input);
