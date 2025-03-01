f3f69250ac717b46b5da0219f33795d822ed99c5
Aiden Fuller <aidenfuller314@gmail.com>
2025-03-01 11:41:40 +1100

    Added support for includeZero hash parameter into inbuilt conditional blocks

--- source/Handlebars/Compiler/Translation/Expression/BoolishConverter.cs
+++ source/Handlebars/Compiler/Translation/Expression/BoolishConverter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using static Expressions.Shortcuts.ExpressionShortcuts;
 
@@ -11,13 +13,24 @@ namespace HandlebarsDotNet.Compiler
         {
             _compilationContext = compilationContext;
         }
-        
+
+        private const string IncludeZero = "includeZero";
+
         protected override Expression VisitBoolishExpression(BoolishExpression bex)
         {
             var condition = Visit(bex.Condition);
             condition = FunctionBuilder.Reduce(condition, _compilationContext, out _);
+            var hashParameters = (HashParametersExpression)VisitHashParametersExpression(bex.HashParameters);
+            // Assert that if there is a hashParameter, it is "includeZero" and has a boolean value
+            if (hashParameters.Parameters.Count > 1 || (hashParameters.Parameters.Count == 1 && !hashParameters.Parameters.ContainsKey(IncludeZero)))
+            {
+                throw new HandlebarsCompilerException($"Boolish expression can only have up to one hash parameter, '{IncludeZero}'");
+            }
+
             var @object = Arg<object>(condition);
-            return Call(() => HandlebarsUtils.IsTruthyOrNonEmpty(@object));
+            var includeZero = Arg<bool>(hashParameters.Parameters.Count == 1 ? hashParameters.Parameters[IncludeZero] : Expression.Constant(false));
+            return Call(() => HandlebarsUtils.IsTruthyOrNonEmpty(@object, includeZero));
+
         }
     }
 }
